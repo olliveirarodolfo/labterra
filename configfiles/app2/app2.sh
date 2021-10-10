e3b0bf1466832013bccc937bd988a79c20614a70
olliveirarodolfo <92041265+olliveirarodolfo@users.noreply.github.com>
2021-10-10 20:38:55 -0300

    Update app2.sh

--- configfiles/app2/app2.sh
+++ configfiles/app2/app2.sh
@@ -6,7 +6,7 @@ apt install apache2 -y
 apt install php libapache2-mod-php -y
 
 # configure site app2
-wget https://github.com/olliveirarodolfo/labterra/raw/main/configfiles/app2/000-default.conf -c /etc/apache2/sites-available/
+wget https://raw.githubusercontent.com/olliveirarodolfo/labterra/main/configfiles/app2/000-default.conf -c /etc/apache2/sites-available/
 wget https://github.com/olliveirarodolfo/labterra/raw/main/configfiles/app2/index.php -c /var/www/html/
 
 systemctl restart apache2.service
