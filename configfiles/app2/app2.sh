65790c2d033e83cf1541d6f1a439413bcb7e4088
olliveirarodolfo <92041265+olliveirarodolfo@users.noreply.github.com>
2021-10-11 13:33:07 -0300

    Add files via upload

--- configfiles/app2/app2.sh
+++ configfiles/app2/app2.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-# install apache
+# install and config Apache
 apt update
 apt install apache2 -y
 apt install php libapache2-mod-php -y
+a2enmod rewrite
 
 # configure site app2
 rm /etc/apache2/sites-available/000-default.conf
 rm /var/www/html/index.*
+rm /var/www/html/.htaccess
 
-if [[ ! -f /etc/apache2/sites-available/000-default.conf ]] || [[ ! -f /var/www/html/index.php ]]
+if [[ ! -f /etc/apache2/sites-available/000-default.conf ]] || [[ ! -f /var/www/html/index.php ]] || [[ ! -f /var/www/html/.htaccess ]]
 then
 /usr/bin/wget https://raw.githubusercontent.com/olliveirarodolfo/labterra/main/configfiles/app2/000-default.conf -O /etc/apache2/sites-available/000-default.conf 
 /usr/bin/wget https://raw.githubusercontent.com/olliveirarodolfo/labterra/main/configfiles/app2/index.php -O /var/www/html/index.php 
+/usr/bin/wget https://raw.githubusercontent.com/olliveirarodolfo/labterra/main/configfiles/app2/.htaccess -O /var/www/html/.htaccess
 fi
 
 systemctl restart apache2.service
